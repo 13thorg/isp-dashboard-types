d79bbdaa76ed09e566a428fb064ac78ddaf7d1eb
sahilkumarsahoo <email.sahil@yahoo.com>
2023-04-02 21:53:21 +0530

    backyo: input interface changed

--- src/types/inputs.interface.ts
+++ src/types/inputs.interface.ts
@@ -1,38 +1,40 @@
 export type InputGroupType = {
-    title: string,
-    description: string,
-    view: GroupViewEnum,
-    inputs: InputType[]
-}
+  title: string;
+  key: string;
+  description: string;
+  view: GroupViewEnum;
+  inputs: InputType[];
+};
 
 export type InputType = {
-    title: string,
-    key: string,
-    type: InputTypeEnum,
-    enum: InputSelectEnumType[],
-    disabled: boolean,
-    errorMessages: string[],
-    value: any,
-    onChange?: (each: InputType, i: number, value: any) => void,
-    onKeyUp?: (each: InputType, i: number, value: any) => void
-}
+  title: string;
+  key: string;
+  type: InputTypeEnum;
+  enum: InputSelectEnumType[];
+  disabled: boolean;
+  errorMessages: string[];
+  zodValidator: any;
+  value: any;
+  onChange?: (each: InputType, i: number, value: any) => void;
+  onKeyUp?: (each: InputType, i: number, value: any) => void;
+};
 
 export type InputSelectEnumType = {
-    key: string,
-    name: string
-}
+  key: string;
+  name: string;
+};
 
 export enum GroupViewEnum {
-    list = 'list',
-    grid = 'grid'
+  list = "list",
+  grid = "grid",
 }
 
 export enum InputTypeEnum {
-    text = 'text',
-    number = 'number',
-    select = 'select',
-    checkbox = 'checkbox',
-    date = 'date',
-    time = 'time',
-    color = 'color',
+  text = "text",
+  number = "number",
+  select = "select",
+  checkbox = "checkbox",
+  date = "date",
+  time = "time",
+  color = "color",
 }
